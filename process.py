cae09ea1738442ff9c28114c9ec0066a8e865f06
stephaniemertz <stephnemertz@gmail.com>
2015-10-24 02:58:56 -0500

    Update process.py
    functional!

--- process.py
+++ process.py
@@ -1,82 +1,73 @@
 
 # coding: utf-8
 
+# In[2]:
+
+global stopwords
+stopwords= ['i','a','about','an','are','as','at','be','by','com','for','from','how','in','is','it','of','on','or','that','the','this','to','was','what','when','where','who','will','with']
+
+
 # In[3]:
 
-from nltk.corpus import stopwords
-
-
-# In[4]:
-
 def tokenize(text, lowercase=True):
-""""
+    """
     Args:
     text: text to tokenize
     lowercase: boolean indicating whether string should be lowercased
 
     Return:
     A list of the words tokenized after splitting on spaces and removing punctuation 
-""""
+    """
 
     # Lowercase string if the flag is set
     if lowercase:
-        sentence = sentence.lower()
+        text = text.lower()
 
     # tokenize and remove punctuation marks from tokens
-    tokenized = sentence.split()
+    tokenized = text.split()
     tokenized = [word.strip('.,?!;:-\\\'/()""') for word in tokenized]
     
-    return tokenized
+    return tokenize
 
 
-# In[ ]:
+# In[4]:
 
-def drop_tokens(tokenized, to_drop, stopwords=True):
-""""
-	Args:
-	tokenized: list of strings
-	to_drop: list of strings 
-	stopwords: boolean
+def drop_tokens(tokenized, usestopwords=True):
+    """
+    Args:
+    tokenized: list of strings
+    stopwords: boolean
 
-	Returns:
-	A list of strings 
+    Returns:
+    A list of strings 
 
-	Removes tokens in to_drop list from tokenized. Allows for removing stopwords with flag
-	Converts the to_drop list to lowercase
-""""
-
-    # Lowercase to_drop
-    td = [n.lower() for n in to_drop]
-
-    # If stopwords is simply set to True, use default stopwords list
-    if stopwords == True:
-        stopwords = stopwords.words('english')
+    Removes stop words from tokenized. 
+    """
+    global stopwords
+    # If stopwords is set to True, use default stopwords list
+    if usestopwords == True:
+        usestopwords = stopwords
     # If set to False, then it's an empty list
     elif stopwords == False:
         stopwords = []
-
-    # Otherwise we know stopwords is a list of strings
-    # Add it to td
-    td += stopwords
     
     # Drop words in td
-    processed_text = [n for n in tokenized if n not in td]
+    processed_text = [n for n in tokenized if n not in stopwords]
 
     return processed_text
-    
-
 
 # In[5]:
 
 def vectorize(text, to_count): 
-"""""
+    """""
     Args:
-    text: list of strings
-    to_count: list of strings
+    text: tokenized text
+    to_count: list of strings,  filler words
 
     Returns:
     A list of floats representing relative frequencies of to_count in text
-"""""
+    Good for graphing if we're doing a web app
+    """""
     vectors = [0.0] * len(to_count) 
     for token in text: 
         if token in to_count:
@@ -94,21 +85,22 @@ def vectorize(text, to_count):
 
 def word_count(tokens, wd_freqs={}):
 
+    """
     Args:
         tokens: list of strings
     wd_freqs: dictionary of {token: frequency} counts
 
     Returns:
     Dictionary of token frequency counts
-
+    """
 
     for t in tokens:
         if t in wd_freqs:
-			wd_freqs[t]+=1
-		else:
-			wd_freqs[t]=1
+            wd_freqs[t]+=1
+        else:
+            wd_freqs[t]=1
 
-	return wd_freqs
+    return wd_freqs
 
 
 # In[ ]:
