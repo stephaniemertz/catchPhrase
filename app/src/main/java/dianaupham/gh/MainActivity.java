3d8024f4aebc04308d7f52a7dc18f34bfb2d70aa
wergi <dianapham2@my.unt.edu>
2015-10-24 08:22:10 -0500

    Format, bug fixes

--- app/src/main/java/dianaupham/gh/MainActivity.java
+++ app/src/main/java/dianaupham/gh/MainActivity.java
@@ -1,6 +1,11 @@
 package dianaupham.gh;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
+import android.util.Log;
 import android.widget.PopupWindow;
 import android.app.Activity;
 import android.content.Context;
@@ -15,11 +20,29 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -56,49 +79,64 @@ public class MainActivity extends Activity implements View.OnClickListener {
         editor.putBoolean("NotLoggedIn", NotLoggedIn);
         editor.putBoolean("AskLogin", AskLogin);
 
-        buttonLogIn = (Button)findViewById(R.id.LogIn); //set login button
+        buttonLogIn = (Button) findViewById(R.id.LogIn); //set login button
         buttonLogIn.setOnClickListener(this); //sets an onClickListener on buttonLogIn
         //if (prefs.getBoolean("NotLoggedIn", false))
         //     buttonLogIn.setVisibility(View.GONE); //removes this button from view, and makes space for others
 
         editor.commit();
-        new CountDownTimer(100,100) {
-            public void onTick(long millisUntilFinished)
-            {
-                if (millisUntilFinished / 500 == 0)
-                {
+        new CountDownTimer(100, 100) {
+            public void onTick(long millisUntilFinished) {
+                if (millisUntilFinished / 500 == 0) {
                     onFinish();
+                } else {
                 }
-                else
-                {}
             }
-            public void onFinish()
-            {
+
+            public void onFinish() {
                 SharedPreferences prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-                if(prefs.getBoolean("NotLoggedIn", true) && prefs.getBoolean("AskLogin", true) ) {
+                if (prefs.getBoolean("NotLoggedIn", true) && prefs.getBoolean("AskLogin", true)) {
                     LoginScreen();
                 }
             }
         }.start();
 
         editor.commit();
-        TextView login_message = (TextView)findViewById(R.id.LoginMessage);
+        TextView login_message = (TextView) findViewById(R.id.LoginMessage);
         NotLogged = prefs.getBoolean("NotLoggedIn", true);
-        if(NotLogged){ login_message.setText("Not logged in"); }
-        else { login_message.setText("Welcome " + prefs.getString("PlayerName", "Player")); }
+        if (NotLogged) {
+            login_message.setText("Not logged in");
+        } else {
+            login_message.setText("Welcome " + prefs.getString("PlayerName", "Player"));
+        }
 
         // get graph view instance
         GraphView graph = (GraphView) findViewById(R.id.graph);
         // set data points
-        BarGraphSeries<DataPoint> barSeries = new BarGraphSeries<DataPoint>(new DataPoint []{
-                new DataPoint(1,6),
-                new DataPoint(2,3),
-                new DataPoint(3,3),
-                new DataPoint(4,2),
-                new DataPoint(5,1)
+        BarGraphSeries<DataPoint> barSeries = new BarGraphSeries<DataPoint>(new DataPoint[]{
+                new DataPoint(1, 6),
+                new DataPoint(2, 3),
+                new DataPoint(3, 3),
+                new DataPoint(4, 2),
+                new DataPoint(5, 1)
         });
+
+     if (NotLogged == false) {
         // draw the graph to screen
+        GridLabelRenderer gridLabel = graph.getGridLabelRenderer();
+        gridLabel.setHorizontalAxisTitle("Words");
+        gridLabel.setVerticalAxisTitle("Frequency");
+        graph.setTitle("Your Speech Patterns");
+        // GraphView 4.x
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+//        staticLabelsFormatter.setHorizontalLabels(new String[] {"old", "middle", "new"});
+//        staticLabelsFormatter.setVerticalLabels(new String[] {"low", "middle", "high"});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
         graph.addSeries(barSeries);  //draw 'barSeries'
+
+        buttonLogIn.setVisibility(View.GONE);
+    }
 }
 
 
@@ -114,7 +152,83 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 new DataPoint(5,1)
         });
         // draw the graph to screen
+        GridLabelRenderer gridLabel = graph.getGridLabelRenderer();
+        gridLabel.setHorizontalAxisTitle("Words");
+        gridLabel.setVerticalAxisTitle("Frequency");
+        graph.setTitle("Your Speech Patterns");
+        // GraphView 4.x
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+//        staticLabelsFormatter.setHorizontalLabels(new String[] {"old", "middle", "new"});
+//        staticLabelsFormatter.setVerticalLabels(new String[] {"low", "middle", "high"});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
         graph.addSeries(barSeries);  //draw 'barSeries'
+    }
+
+    //++++++++++++++++++++++++++++++++WEBSITE JSON TO ANDROID+++++++++++++++++++++++++++++++//
+    //http://www.codeproject.com/Articles/267023/Send-and-receive-json-between-android-and-php
+    public void GetTranscription (View v) {
+        try {
+            // http://androidarabia.net/quran4android/phpserver/connecttoserver.php
+
+            // Log.i(getClass().getSimpleName(), "send  task - start");
+            HttpParams httpParams = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(httpParams, 500);
+            HttpConnectionParams.setSoTimeout(httpParams, 500);
+            HttpParams p = new BasicHttpParams();
+            // p.setParameter("name", pvo.getName());
+            p.setParameter("user", "1");
+
+            // Instantiate an HttpClient
+            HttpClient httpclient = new DefaultHttpClient(p);
+            String url = "http://10.0.2.2:8080/sample1/" +
+                    "webservice1.php?user=1&format=json";
+            HttpPost httppost = new HttpPost(url);
+
+            // Instantiate a GET HTTP method
+            try {
+                Log.i(getClass().getSimpleName(), "send  task - start");
+                //
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
+                        2);
+                nameValuePairs.add(new BasicNameValuePair("user", "1"));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                String responseBody = httpclient.execute(httppost,
+                        responseHandler);
+                // Parse
+                JSONObject json = new JSONObject(responseBody);
+                JSONArray jArray = json.getJSONArray("posts");
+                ArrayList<HashMap<String, String>> mylist =
+                        new ArrayList<HashMap<String, String>>();
+
+                for (int i = 0; i < jArray.length(); i++) {
+                    HashMap<String, String> map = new HashMap<String, String>();
+                    JSONObject e = jArray.getJSONObject(i);
+                    String s = e.getString("post");
+                    JSONObject jObject = new JSONObject(s);
+
+                    map.put("idusers", jObject.getString("idusers"));
+                    map.put("UserName", jObject.getString("UserName"));
+                    map.put("FullName", jObject.getString("FullName"));
+
+                    mylist.add(map);
+                }
+                Toast.makeText(this, responseBody, Toast.LENGTH_LONG).show();
+
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            // Log.i(getClass().getSimpleName(), "send  task - end");
+
+        } catch (Throwable t) {
+            Toast.makeText(this, "Request failed: " + t.toString(),
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
     private void LogInClick() {   //LogIn method
