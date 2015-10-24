8192350da26a79d9aaed6cc7fe79cc0de2a279c6
wergi <dianapham2@my.unt.edu>
2015-10-24 10:31:06 -0500

    WOrking sign up\!

--- app/src/main/java/dianaupham/gh/MainActivity.java
+++ app/src/main/java/dianaupham/gh/MainActivity.java
@@ -1,5 +1,8 @@
 package dianaupham.gh;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,20 +32,34 @@ import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import android.util.Log;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.util.concurrent.ExecutionException;
+import org.apache.http.HttpStatus;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -137,6 +154,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         buttonLogIn.setVisibility(View.GONE);
     }
+
+    View thisactivity = this.findViewById(android.R.id.content).getRootView();
+    clickbuttonRecieve(thisactivity);
 }
 
 
@@ -165,70 +185,72 @@ public class MainActivity extends Activity implements View.OnClickListener {
         graph.addSeries(barSeries);  //draw 'barSeries'
     }
 
+
     //++++++++++++++++++++++++++++++++WEBSITE JSON TO ANDROID+++++++++++++++++++++++++++++++//
     //http://www.codeproject.com/Articles/267023/Send-and-receive-json-between-android-and-php
-    public void GetTranscription (View v) {
+    public void clickbuttonRecieve(View v) {
         try {
-            // http://androidarabia.net/quran4android/phpserver/connecttoserver.php
-
-            // Log.i(getClass().getSimpleName(), "send  task - start");
+            JSONObject json = new JSONObject();
+            json.put("UserName", "test2");
+            json.put("FullName", "1234567");
             HttpParams httpParams = new BasicHttpParams();
-            HttpConnectionParams.setConnectionTimeout(httpParams, 500);
+            HttpConnectionParams.setConnectionTimeout(httpParams,
+                    500);
             HttpConnectionParams.setSoTimeout(httpParams, 500);
-            HttpParams p = new BasicHttpParams();
-            // p.setParameter("name", pvo.getName());
-            p.setParameter("user", "1");
+            HttpClient client = new DefaultHttpClient(httpParams);
+            //
+            String url3 = "http://stephaniemertz.com/backend/signup.php?" +
+                         "json={\"UserName\":1,\"FullName\":2}";
+            String url2 = "http://10.0.2.2:8080/sample1/webservice2.php";
+            String url = "http://stephaniemertz.com/backend/signup.php?message=username&email=secretpassword";
 
-            // Instantiate an HttpClient
-            HttpClient httpclient = new DefaultHttpClient(p);
-            String url = "http://10.0.2.2:8080/sample1/" +
-                    "webservice1.php?user=1&format=json";
-            HttpPost httppost = new HttpPost(url);
+            //String sendCommand = "http://" + remoteIPAddress + ":1999/ST:" +
+                    //partID.get(current.getDPID()) + current.getDeviceID() + ":END";
+            String receive = new connectTask().execute(url).get();
 
-            // Instantiate a GET HTTP method
-            try {
-                Log.i(getClass().getSimpleName(), "send  task - start");
-                //
-                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
-                        2);
-                nameValuePairs.add(new BasicNameValuePair("user", "1"));
-                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                ResponseHandler<String> responseHandler = new BasicResponseHandler();
-                String responseBody = httpclient.execute(httppost,
-                        responseHandler);
-                // Parse
-                JSONObject json = new JSONObject(responseBody);
-                JSONArray jArray = json.getJSONArray("posts");
-                ArrayList<HashMap<String, String>> mylist =
-                        new ArrayList<HashMap<String, String>>();
+            /*HttpPost request = new HttpPost(url);
+            request.setEntity(new ByteArrayEntity(json.toString().getBytes(
+                    "UTF8")));
+            request.setHeader("json", json.toString());
+            HttpResponse response = client.execute(request);
+            HttpEntity entity = response.getEntity();
+            // If the response does not enclose an entity, there is no need
+            if (entity != null) {
+                InputStream instream = entity.getContent();
 
-                for (int i = 0; i < jArray.length(); i++) {
-                    HashMap<String, String> map = new HashMap<String, String>();
-                    JSONObject e = jArray.getJSONObject(i);
-                    String s = e.getString("post");
-                    JSONObject jObject = new JSONObject(s);
-
-                    map.put("idusers", jObject.getString("idusers"));
-                    map.put("UserName", jObject.getString("UserName"));
-                    map.put("FullName", jObject.getString("FullName"));
-
-                    mylist.add(map);
-                }
-                Toast.makeText(this, responseBody, Toast.LENGTH_LONG).show();
-
-            } catch (ClientProtocolException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            // Log.i(getClass().getSimpleName(), "send  task - end");
-
+                String result = convertStreamToString(instream);
+                Log.i("Read from server", result);
+                Toast.makeText(this,  result,
+                        Toast.LENGTH_LONG).show();
+            }*/
+            Toast.makeText(this, receive, Toast.LENGTH_LONG).show();
         } catch (Throwable t) {
             Toast.makeText(this, "Request failed: " + t.toString(),
                     Toast.LENGTH_LONG).show();
+
         }
+    }
+
+    private static String convertStreamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 
     private void LogInClick() {   //LogIn method
@@ -316,5 +338,71 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    /* This class conducts networking that needs to be separate from the Main thread
+	 * The AsyncTask runs when new connectTask.execute(String message) is called
+	 * It creates a new HttpURLConnection to send the request and
+	 * returns a String of the received message from the server
+	 * The returned string can be retrieved by calling get() on the connectTask object
+	 */
+    public class connectTask extends AsyncTask<String,String,String> {
+        protected String doInBackground(String... message) {
+            HttpURLConnection urlConnection = null;
+            String receive = "";
+
+            try {
+                URL url = new URL(message[0]);
+                Log.d("Sending:", message[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                int responseCode = urlConnection.getResponseCode();
+
+                if(responseCode == HttpStatus.SC_OK){
+                    receive = readStream(urlConnection.getInputStream());
+                    Log.d("Receive: ", receive);
+                }else{
+                    Log.v("connectTask", "Response code:"+ responseCode);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if(urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+
+            return receive;
+        }
+
+        /* Helper method for processing the server return input
+         * from http://syntx.io/how-to-send-an-http-request-from-android-using-httpurlconnection/
+         */
+        private String readStream(InputStream in) {
+            BufferedReader reader = null;
+            StringBuffer response = new StringBuffer();
+            try {
+                reader = new BufferedReader(new InputStreamReader(in));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return response.toString();
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
     }
 }
